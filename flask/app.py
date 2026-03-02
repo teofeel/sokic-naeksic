77202d39e52ec8491366230442355c894ce65ff6
teofeel <teodor.perun@gmail.com>
2026-03-02 15:15:08 +0100

    #20 Created main-view template

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template_string, render_template
 from core.sokic.core.use_cases.plugin_loader import PluginLoader
 loader = PluginLoader()
 loader.load_all()
 
-app = Flask(__name__)
+
+import os
+from pathlib import Path
+current_dir = Path(__file__).resolve().parent
+template_path = current_dir.parent / 'core' / 'templates'
+app = Flask(__name__, template_folder=str(template_path))
 
 
 # @app.route("/")
@@ -28,16 +33,18 @@ def test_visualizer():
     graph_html = visualizer.visualize(graph_model)
     # print(graph_html)
 
-    return render_template_string("""
-        <!DOCTYPE html>
-        <html>
-        <head><title>Graph Test</title></head>
-        <body>
-            <h1>Graph Visualization Test</h1>
-            {{ graph_content | safe }}
-        </body>
-        </html>
-    """, graph_content=graph_html)
+    return render_template('main-view.html', plugin_html=graph_html)
+
+    #return render_template_string("""
+    #    <!DOCTYPE html>
+    #    <html>
+    #    <head><title>Graph Test</title></head>
+    #    <body>
+    #        <h1>Graph Visualization Test</h1>
+    #        {{ graph_content | safe }}
+    #    </body>
+    #    </html>
+    #""", graph_content=graph_html)
 
 
 if __name__ == "__main__":
