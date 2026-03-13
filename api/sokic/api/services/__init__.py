70051fce9a7176674db39472dbce1a4e56c87f86
isidorakorda <isidorakorda24@gmail.com>
2026-03-13 11:21:04 +0100

    Fixed init file for api

--- api/sokic/api/services/__init__.py
+++ api/sokic/api/services/__init__.py
@@ -2,4 +2,6 @@ from .data_source_plugin import DataSourcePlugin
 from .search_service import SearchService
 from .filter_service import FilterService
 from .workspace_repository import WorkspaceRepository
-__all__ = ["DataSourcePlugin","SearchService", "FilterService", "WorkspaceRepository"]
+from .plugin import Plugin
+from .visualizer_plugin import VisualizerPlugin
+__all__ = ["DataSourcePlugin","SearchService", "FilterService", "WorkspaceRepository", "Plugin", "VisualizerPlugin"]
